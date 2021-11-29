0cb7ede3ed3a7ab89f9be9915281b99a56fb65c7
Armen <armen@mail.ru>
2021-11-29 05:04:06 +0400

    Good night...

--- main.js
+++ main.js
@@ -12,4 +12,5 @@ const some = (pram1, pram2, pram3)=>{
 // The last try in about in storm/1 branch
 // The very last try in storm in storm/1 branch........
 // Go bed :) (in about - storm/1)
+// Good night... (in content - storm/1)
 some("Devs", "and", "Designers")
